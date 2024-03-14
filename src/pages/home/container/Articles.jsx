87fdbc079101c17c2d2704405a562520f398f06d
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-03-14 23:53:25 +0200

    Implement Redux.

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import ArticleCard from "../../../components/ArticleCard";
+import { useSelector, useDispatch } from "react-redux";
+import { changeCount } from "../../../store/actions/countActions";
 
 const Articles = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  const countChangeHandler = (type) => {
+    dispatch(changeCount(type));
+  };
+
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
       <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
@@ -13,6 +22,12 @@ const Articles = () => {
         <span>Mai multe trasee</span>
         <FaArrowRight className="w-3" />
       </button>
+      {/* Just for testing stuff */}
+      <div className="mt-2 flex items-center gap-x-5">
+        <button onClick={() => countChangeHandler("DECREASE")}>decrease</button>
+        {count.number}
+        <button onClick={() => countChangeHandler("INCREASE")}>increase</button>
+      </div>
     </section>
   );
 };
