acec1d7214689a8af3b07f7887d03784eb4a5a03
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-03-15 22:05:28 +0200

    Save user data in local storage and redirect connected user to homepage.

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import ArticleCard from "../../../components/ArticleCard";
-import { useSelector, useDispatch } from "react-redux";
-import { changeCount } from "../../../store/actions/countActions";
 
 const Articles = () => {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
-
-  const countChangeHandler = (type) => {
-    dispatch(changeCount(type));
-  };
-
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
       <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
@@ -23,11 +14,6 @@ const Articles = () => {
         <FaArrowRight className="w-3" />
       </button>
       {/* Just for testing stuff */}
-      <div className="mt-2 flex items-center gap-x-5">
-        <button onClick={() => countChangeHandler("DECREASE")}>decrease</button>
-        {count.number}
-        <button onClick={() => countChangeHandler("INCREASE")}>increase</button>
-      </div>
     </section>
   );
 };
