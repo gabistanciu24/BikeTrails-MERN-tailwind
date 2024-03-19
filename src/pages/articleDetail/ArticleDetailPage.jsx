3a28ff4d4793cbfc5e5c36d006153e5ae0e6b90a
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-03-19 16:03:04 +0200

    creating loading & error state

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -15,6 +15,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getSinglePost } from "../../services/index/posts";
 import { generateHTML } from "@tiptap/react";
 import parse from "html-react-parser";
+import ArticleDetailSkeleton from "./components/ArticleDetailSkeleton";
+import ErrorMessage from "../../components/ErrorMessage";
 
 const postsData = [
   {
@@ -50,7 +52,7 @@ const ArticleDetailPage = () => {
   const [breadCrumbsData, setbreadCrumbsData] = useState([]);
   const [body, setBody] = useState(null);
 
-  const { data } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryFn: () => getSinglePost({ slug }),
     queryKey: ["trail", slug],
     onSuccess: (data) => {
@@ -69,55 +71,61 @@ const ArticleDetailPage = () => {
 
   return (
     <MainLayout>
-      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
-        <article className="flex-1">
-          <BreadCrumbs data={breadCrumbsData} />
-          <img
-            className="rounded-xl w-full"
-            src={
-              data?.photo
-                ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
-                : images.Post1
-            }
-            alt={data?.title}
-          />
-          <div className="mt-4 flex gap-2">
-            {" "}
-            {data?.categories.map((category) => (
-              <Link
-                to={`/trail?category=${category.name}`}
-                className="text-primary text-sm font-roboto inline-block md:text-base"
-              >
-                {category.name}
-              </Link>
-            ))}
-          </div>
-          <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[1.625rem]">
-            {data?.title}
-          </h1>
-          <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
-          <CommentsContainer className="mt-10" logginedUserId="a" />
-        </article>
-        <div>
-          <SuggestedPosts
-            header="Ultimele trasee"
-            posts={postsData}
-            tags={tagsData}
-            className="mt-8 lg:mt-0 lg:max-w-xs"
-          />
-          <div className="mt-7">
-            <h2 className="font-roboto font-medium text-dark-hark mb-4 md:text-xl">
-              Distribuie pe:
-            </h2>
-            <SocialShareButtons
-              url={encodeURI(
-                `https://www.linkedin.com/in/gabriel-stanciu-b66482268/`
-              )}
-              title={encodeURIComponent("Stanciu Gabriel LinkedIn")}
+      {isLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Couldn't fetch the post detail." />
+      ) : (
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-1">
+            <BreadCrumbs data={breadCrumbsData} />
+            <img
+              className="rounded-xl w-full"
+              src={
+                data?.photo
+                  ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
+                  : images.Post1
+              }
+              alt={data?.title}
             />
+            <div className="mt-4 flex gap-2">
+              {" "}
+              {data?.categories.map((category) => (
+                <Link
+                  to={`/trail?category=${category.name}`}
+                  className="text-primary text-sm font-roboto inline-block md:text-base"
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+            <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[1.625rem]">
+              {data?.title}
+            </h1>
+            <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+            <CommentsContainer className="mt-10" logginedUserId="a" />
+          </article>
+          <div>
+            <SuggestedPosts
+              header="Ultimele trasee"
+              posts={postsData}
+              tags={tagsData}
+              className="mt-8 lg:mt-0 lg:max-w-xs"
+            />
+            <div className="mt-7">
+              <h2 className="font-roboto font-medium text-dark-hark mb-4 md:text-xl">
+                Distribuie pe:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI(
+                  `https://www.linkedin.com/in/gabriel-stanciu-b66482268/`
+                )}
+                title={encodeURIComponent("Stanciu Gabriel LinkedIn")}
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </MainLayout>
   );
 };
