7166c0a7c146f23e9c95370d06ee6c8661b282b2
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-03-25 12:09:37 +0200

    add a new post

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import ProfilePage from "./pages/profile/ProfilePage.jsx";
 import AdminLayout from "./pages/admin/AdminLayout.jsx";
 import Admin from "./pages/admin/screens/Admin.jsx";
 import Comments from "./pages/admin/screens/comments/Comments.jsx";
-import NewPost from "./pages/admin/screens/posts/NewPost.jsx";
 import ManagePosts from "./pages/admin/screens/posts/ManagePosts.jsx";
 import EditPost from "./pages/admin/screens/posts/EditPost.jsx";
 
@@ -25,7 +24,6 @@ function App() {
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<Admin />} />
           <Route path="comments" element={<Comments />} />
-          <Route path="posts/new" element={<NewPost />} />
           <Route path="posts/manage" element={<ManagePosts />} />
           <Route path="posts/manage/edit/:slug" element={<EditPost />} />
         </Route>
--- src/pages/admin/components/header/NavItemCollapse.jsx
+++ src/pages/admin/components/header/NavItemCollapse.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavItemCollapse = ({
   title,
-  content,
+  children,
   icon,
   name,
   activeNavName,
@@ -40,11 +40,7 @@ const NavItemCollapse = ({
           {title}
         </div>
         <div className="d-collapse-content">
-          <div className="mt-2 flex flex-col gap-y-2">
-            {content.map((item) => (
-              <Link to={item.link}>{item.title}</Link>
-            ))}
-          </div>
+          <div className="mt-2 flex flex-col gap-y-2">{children}</div>
         </div>
       </div>
     </div>
