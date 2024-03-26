c912b5dbf771bf7961d59ac032257d662c169466
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-03-26 20:45:48 +0200

    Comments management, fetch comments in Admin Dashboard.

--- backend/controllers/commentControllers.js
+++ backend/controllers/commentControllers.js
@@ -61,3 +61,61 @@ export const deleteComment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getAllComments = async (req, res, next) => {
+  try {
+    const filter = req.query.searchKeyword;
+    let where = {};
+    if (filter) {
+      where.desc = { $regex: filter, $options: "i" };
+    }
+    let query = Comment.find(where);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * pageSize;
+    const total = await Comment.find(where).countDocuments();
+    const pages = Math.ceil(total / pageSize);
+
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
+    if (page > pages) {
+      return res.json([]);
+    }
+    const result = await query
+      .skip(skip)
+      .limit(pageSize)
+      .populate([
+        {
+          path: "user",
+          select: ["avatar", "name", "verified"],
+        },
+        {
+          path: "parent",
+          populate: [
+            {
+              path: "user",
+              select: ["avatar", "name"],
+            },
+          ],
+        },
+        {
+          path: "replyOnUser",
+          select: ["avatar", "name"],
+        },
+        {
+          path: "post",
+          select: ["slug", "title"],
+        },
+      ])
+      .sort({ updatedAt: "desc" });
+    return res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -3,12 +3,16 @@ import {} from "../controllers/postControllers";
 import {
   createComment,
   deleteComment,
+  getAllComments,
   updateComment,
 } from "../controllers/commentControllers";
-import { authGuard } from "../middleware/authMiddleware";
+import { adminGuard, authGuard } from "../middleware/authMiddleware";
 const router = express.Router();
 
-router.post("/", authGuard, createComment);
+router
+  .route("/")
+  .post(authGuard, createComment)
+  .get(authGuard, adminGuard, getAllComments);
 router
   .route("/:commentId")
   .put(authGuard, updateComment)
